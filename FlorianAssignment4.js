a68950a373947b0740b98026ea5dc8178dbe4d80
FKuhlewind <florian.kuhlewind@gmail.com>
2014-03-02 13:31:04 +0100

    Update FlorianAssignment4.js

--- FlorianAssignment4.js
+++ FlorianAssignment4.js
@@ -28,6 +28,10 @@ xData = vibData[xD].slice(0,9);
 xLabel = vibData[xD].slice(10);
 yData = vibData[yD].slice(0,9);
 yLabel = vibData[yD].slice(10);
+// possibly shorten this later:
+xyData = [xData, yData];
+xyLabel = [xLabel,yLabel];
+
 
 //create scale functions
 var xScale = d3.scale.linear()
@@ -56,19 +60,30 @@ var mySVG = d3.select("body")
     .attr("height", height);    
 //Create circles
 mySVG.selectAll("circle")
-    .data(myObject.events)
+    .data(xyData)
     .enter()
-    .append("rect")
-    .attr("y", function(d, i) {
-                 return yScale(i);})
-    .attr("x", function(d) {
-            return xScale(0);})
-    .attr("height", yScale.rangeBand())
-    .attr("width", function(d) {
-                 return xScale(d.rating)-padding;})
-    .style("fill", function (d) {
-            if (!d.ficticious) {return "#ff6666"}
-            else {return "#8180ff"} ;});   
+    .append("circle")
+    .attr("cx", function(d) {
+                return xScale(d[0]);})
+    .attr("cy", function(d) {
+            	return yScale(d[1]);})
+    .attr("r", 5);
+
+//create X axis
+mySVG.append("g")
+    .attr("transform", "translate(0," + (height - padding) + ")")
+    .style({ 'stroke': 'Black', 'fill': 'none', 'stroke-width': '1px'})
+    .call(xAxis);
+//create Y axis
+mySVG.append("g")
+    .attr("transform", "translate(" + padding + ",0)")
+    .style({ 'stroke': 'Black', 'fill': 'none', 'stroke-width': '1px'})
+    .call(yAxis);
+
+    
+    
+    
+    
 // add labels
 mySVG.selectAll("text")
    .data(myObject.events)
@@ -83,37 +98,4 @@ mySVG.selectAll("text")
             return xScale(0)+10;})
     .attr("alignment-baseline" , "middle")
     .style({ 'stroke': 'Black', 'fill': 'black', 'stroke-width': '0.5px' });
-//create X axis
-mySVG.append("g")
-    .attr("transform", "translate(0," + (height - padding) + ")")
-    .style({ 'stroke': 'Black', 'fill': 'none', 'stroke-width': '1px'})
-    .call(xAxis);
-    
-    
-//// delete later
-[
-  
-  [1.64, 0.76, 0.64,  0, 0.53, 0.6, 0.53, 0.98, 1.34, 0.37, "Vibrato Time [s]"]
-,
-  
-  [10.08, -4.1, -4.25, -3.1, 18.82, 2, 28.95, -41.8, -6.1, 1.4, "Pitch Deviation [Hz]"]
-,
-  
-  [9.5, 4, 3.5, 0, 3, 3.5, 3.5, 4.5, 8, 2.5, "Number of Cycles"]
-,
-  
-  [372, 293, 277, 493, 594, 370, 447, 287, 369, 494, "Mean Pitch [Hz]"]
-,
-  
-  [5.95, 6.41, 5.96, 0, 7.13, 7.36, 6.73, 5.85, 5.96, 6.7, "Rate [Hz]"]
-,
- 
-  [92.55, 45.98, 86.18, 0, 56.94, 40.42, 79.81, 82.76, 62.12, 67.26, "Extent [Hz]"]
-,
-  
-  [0.53, 0.38, 0.57, 0, 0.83, 0.57,  0.43, 0.44, 0.19, 0.8, "Max Extent Position [s]"]
-,
-  
-  [1.89, 0.76, 1.06, 0.46, 0.53, 0.6, 0.65, 0.98, 1.51, 0.72, "Duration [s]"]
-]
 
