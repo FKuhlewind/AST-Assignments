1359bc05b341445c7d748e2d717f2485534cee9e
FKuhlewind <florian.kuhlewind@gmail.com>
2014-03-06 23:36:44 +0100

    Update FlorianAssignment5b.js

--- FlorianAssignment5b.js
+++ FlorianAssignment5b.js
@@ -1,7 +1,7 @@
 // define basic values
-var width = 500;
-var height = 500;
-var padding = 80;
+var width = 600;
+var height = 600;
+var padding = 90;
 var addToAxis = 0.1; 
 xD = 4; yD = 5; // define values to display initially
 myDataRef = new Firebase('https://vib-data.firebaseio.com/');
@@ -57,7 +57,6 @@ function createForm () {
 		vibData = data; 
 		vL = vibData.length;
 		firS = ''; secS = '';
-		
 		startS = '<form style="display:inline" id="myForm"><br/><u>Select values to be displayed: </u><br/><p style="display:inline">x-Axis:</p><select id="xAxisChoice">';
 		midS = '</select><p style="display:inline">    y-Axis:   </p><select id="yAxisChoice">';
 		endS = '</select><br/><a><i id="update">Click this text to update diagram</i></a></form>';
@@ -72,7 +71,7 @@ function createForm () {
 		finalS = startS + firS + midS + secS + endS;
 		$('body').append(finalS);
 		
-		////////// enable update start
+		////////// enable update
 		
 		d3.select("#update").on("click", function() {
 	
@@ -172,23 +171,20 @@ d3.json('https://vib-data.firebaseio.com/.json', function(data) {
 	mySVG.append("text")
 	  .attr("class", "x text")
           .attr("x", width/2 )
-          .attr("y", height-(padding/2) )
+          .attr("y", height-(padding/3) )
           .style("text-anchor", "middle")
           .text(xLabel)
           .style({ 'stroke': 'Black', 'fill': 'black', 'stroke-width': '0.5px' });
 	mySVG.append("text")
 	  .attr("class", "y text")
 	  .text(yLabel)
-          .attr("x", padding/2)
+          .attr("x", padding/3)
           .attr("y", height/2)
           .style("text-anchor", "middle")
           .style({ 'stroke': 'Black', 'fill': 'black', 'stroke-width': '0.5px' })
           .style("writing-mode", "tb");
 
-	//aooend update choice options
-	//var myDrop='<form style="display:inline" id="myForm"><br/><u>Select values to be displayed: </u><br/><div><p style="display:inline">x-Axis:    </p><select id="xAxisChoice"><option value=0>Vibrato Time</option><option value=1>Pitch Deviation</option><option value=2>Number of cycles</option><option value=3>Mean Pitch</option><option selected value=4>Rate</option><option value=5>Extent</option><option value=6>Mx Extent Position</option><option value=7>Duration</option></select><p style="display:inline">    y-Axis:   </p><select id="yAxisChoice"><option value=0>Vibrato Time</option><option value=1>Pitch Deviation</option><option value=2>Number of cycles</option><option value=3>Mean Pitch</option><option value=4>Rate</option><option selected value=5>Extent</option><option value=6>Mx Extent Position</option><option value=7>Duration</option></select><br/><a><i id="update">Click this text to update diagram</i></a></div></form>'
-	//$('body').append(myDrop);
-	
+	// creates update form
 	createForm();
 	
 	var newValuesForm='<form id="newValues"><br/><u>Enter new Values to be stored: </u><br/>01: <input type="number" id="val1" style="width: 50px;">02: <input type="number" id="val2" style="width: 50px;">03: <input type="number" id="val3" style="width: 50px;">04: <input type="number" id="val4" style="width: 50px;">05: <input type="number" id="val5" style="width: 50px;"><br/>06: <input type="number" id="val6" style="width: 50px;">07: <input type="number" id="val7" style="width: 50px;">08: <input type="number" id="val8" style="width: 50px;">09: <input type="number" id="val9" style="width: 50px;">10: <input type="number" id="val10" style="width: 50px;"><br/>Data type and unit (e.g. <i>Rate [Hz]</i>):<input type="text" id="newLabel" ><br/><a><i id="storeData">Click this text to store new data</i></a></form>';
@@ -215,62 +211,6 @@ d3.json('https://vib-data.firebaseio.com/.json', function(data) {
 				vibData = data; 
 				alert("New data stored successfully, you can now select and display it above");
 				});
-	
-	
 		createForm();
-		/// update fropdown menu with handlebars
-		/// remove values from form
-				
 		});
-
-	//update with new data on click
-	/*d3.select("#update")
-		.on("click", function() {
-	
-		//get JSON data
-		d3.json('https://vib-data.firebaseio.com/.json', function(data) {
-		
-		vibData = data;
-							
-		// get info on which values to display on Axis
-		xD = document.getElementById("xAxisChoice").value;
-		yD = document.getElementById("yAxisChoice").value;
-	
-		getDisplayRange();
-		getData();
-		updateScaleDomains();
-		defineXYaxis();
-	
-		//update circles
-		mySVG.selectAll("circle")
-	    		.data(xyData)
-	    		.transition()
-	    		.duration(1000)
-	    		.attr("cx", function(d) {
-	                	return xScale(d[0]);})
-	    		.attr("cy", function(d) {
-	            		return yScale(d[1]);});
-	            
-		//update x and y axis
-		mySVG.select(".x.axis")
-	    		.transition()
-	    		.duration(500)
-	    		.call(xAxis);
-	    	mySVG.select(".y.axis")
-	    		.transition()
-	    		.duration(500)
-	    		.call(yAxis);
-	
-		//update text labels
-		mySVG.select(".x.text")
-			.text(xLabel)
-	    		.transition()
-	    		.duration(1000);
-	    		
-	    	mySVG.select(".y.text")
-	    		.transition()
-	    		.duration(1000)
-	    		.text(yLabel);
-		});
-	}); */
-});
+	});
